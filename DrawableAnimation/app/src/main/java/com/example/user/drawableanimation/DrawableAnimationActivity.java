82e5eca9b575ca8bb8f56817ace5756d4705bf08
qjxjdhkdwk95 <tmdxo2110@naver.com>
2017-05-10 20:25:41 +0900

    test commit

---
+++ DrawableAnimation/app/src/main/java/com/example/user/drawableanimation/DrawableAnimationActivity.java
@@ -0,0 +1,28 @@
+package com.example.user.drawableanimation;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.animation.Animation;
+import android.widget.ImageView;
+
+public class DrawableAnimationActivity extends AppCompatActivity {
+    AnimationDrawable rocketAnimation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_drawable_animation);
+        ImageView rocketImage = (ImageView) findViewById(R.id.rocket_image);
+     rocketAnimation = (AnimationDrawable) rocketImage.getBackground();
+    }
+
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            rocketAnimation.start();
+            return true;
+        }
+        return super.onTouchEvent(event);
+    }
+}
